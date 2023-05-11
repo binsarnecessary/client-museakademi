099dcc0a22e3d88200a43714a84195f4e1a64850
candraf <candrafriyandi52@gmail.com>
2023-05-12 00:21:32 +0700

    update perbaikan halaman transaksi

--- src/pages/ProfileUser/Transaction.js
+++ src/pages/ProfileUser/Transaction.js
@@ -4,6 +4,7 @@ import example from '../../assets/image/course-img1.jpg'
 import './Transaction.css';
 import { ButtonGroup, Tab, Tabs, Button, ListGroup } from 'react-bootstrap';
 
+
 const data = [
     {
         id: 1,
@@ -56,10 +57,9 @@ const data = [
 ];
 
 
-
-
 const Transaction = () => {
-    const [filter, setFilter] = useState('Semua');
+    const [filter, setFilter] = useState('semua');
+    const [activeButton, setActiveButton] = useState('semua');
     const handleFilter = (filter) => {
         setFilter(filter); // set filter yang dipilih
     };
@@ -69,25 +69,23 @@ const Transaction = () => {
         }
         return item.status.toLowerCase() === filter.toLowerCase();
     });
+
     return (
         <>
             <Navbar />
             <div class='container'>
-            <h1 class='mt-5'>Riwayat Transaksi</h1>
+                <h3 class='mt-5' >Riwayat Transaksi</h3>
+                <p class="small text-muted" style={{ marginBottom: '2rem' }}>
+                    Halaman ini memuat transaksi yang pernah Anda lakukan.
+                </p>
             </div>
 
-            <div class="container">
-                <div class="row border bold mb-2">
-                    <div class="col">
-                        <div class="btn-group" >
-                            <button type="button active" className={`btn mr-2 ${filter === 'Semua' ? 'active' : ''}`} onClick={() => handleFilter('semua')} >Semua </button>
-                            <button type="button" className={`btn mr-2`} onClick={() => handleFilter('menunggu pembayaran')}>Menunggu Pembayaran</button>
-                            <button type="button" className={`btn mr-2`} onClick={() => handleFilter('menunggu konfirmasi')}>Menunggu Konfirmasi</button>
-                            <button type="button" className={`btn mr-2`} onClick={() => handleFilter('selesai')}>Selesai</button>
-                            <button type="button" className={`btn `} onClick={() => handleFilter('tidak valid')}>Tidak valid</button>
-                        </div>
-                    </div>
-                </div>
+            <div className='container' style={{ marginBottom: '2rem' }}>
+                <button className={`btn-custom ${activeButton === 'semua' ? 'active' : ''}`} onClick={() => handleFilter('semua')} >Semua</button>
+                <button className={`btn-custom ${activeButton === 'menunggu pembayaran' ? 'active' : ''}`} onClick={() => handleFilter('menunggu pembayaran')}>Menunggu Pembayaran</button>
+                <button className={`btn-custom ${activeButton === 'menunggu konfirmasi' ? 'active' : ''}`} onClick={() => handleFilter('menunggu konfirmasi')}>Menunggu Konfirmasi</button>
+                <button className={`btn-custom ${activeButton === 'selesai' ? 'active' : ''}`} onClick={() => handleFilter('selesai')}>Selesai</button>
+                <button className={`btn-custom ${activeButton === 'tidak valid' ? 'active' : ''}`} onClick={() => handleFilter('tidak valid')}>Tidak Valid</button>
             </div>
 
 
@@ -97,19 +95,17 @@ const Transaction = () => {
                         <div class="card mb-3" >
                             <div class="row no-gutters align-items-center">
                                 <div class="col-md-3 d-none d-md-block">
-                                    <img src={item.image} style={{ borderRadius: '20px', width: '15rem' }} alt="cover" />
+                                    <img src={item.image} style={{ width: '12rem', marginLeft: '2rem' }} alt="cover" />
                                 </div>
                                 <div class="col-12 col-md-9">
                                     <div class="card-body d-md-flex align-items-center justify-content-between">
                                         <div >
                                             <h6 class="card-title text-truncate w-100">{item.judul}</h6>
                                             <div class="d-flex align-items-center mb-2">
-
                                                 <span class="mx-2 small">{item.invoice}</span>
                                             </div>
                                             <div class="mb-2">
-                                                {`Rp ${item.price}`}
-                                                <span class="text-muted small"><del>Rp 398.000</del></span>
+                                                {`Rp. ${item.price}`}
                                             </div>
                                             <div>
                                                 <span class={`badge ${item.status === 'menunggu pembayaran' ? 'badge-warning' : item.status === 'menunggu konfirmasi' ? 'badge-info' : item.status === 'selesai' ? 'badge-success' : 'badge-danger'}`}>
